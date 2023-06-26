d87e9573f5c9494635a858f80938bac8c606a131
uw4 <uli.walther@gmail.com>
2023-06-26 18:49:38 +0200

    Save error code + error attributes

--- src/main/java/com/dashjoin/jsonata/JException.java
+++ src/main/java/com/dashjoin/jsonata/JException.java
@@ -3,14 +3,26 @@ package com.dashjoin.jsonata;
 import java.util.List;
 
 public class JException extends Exception {
+
+    String error;
+    int location;
+    Object current;
+    Object expected;
+
     public JException(String error, int location) {
         super("JSonataException "+error+" @"+location);
+        this.error = error; this.location = location;
     }
     public JException(String error, int location, Object currentToken) {
         super("JSonataException "+error+" @"+location+" current="+currentToken);
+        this.error = error; this.location = location;
+        this.current = currentToken;
     }
     public JException(String error, int location, Object currentToken, Object expected) {
         super("JSonataException "+error+" @"+location+" current="+currentToken+" expected="+expected);
+        this.error = error; this.location = location;
+        this.current = currentToken;
+        this.expected = expected;
     }
 
     // Recover
