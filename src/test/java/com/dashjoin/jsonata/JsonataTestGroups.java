7e630c737f5cddc2059868c539222a5cc10de3d3
uw4 <uli.walther@gmail.com>
2023-06-26 18:52:33 +0200

    Explicitly define Jsonata test groups

---
+++ src/test/java/com/dashjoin/jsonata/JsonataTestGroups.java
@@ -0,0 +1,406 @@
+package com.dashjoin.jsonata;
+
+import org.junit.Test;
+
+public class JsonataTestGroups extends JsonataTest {
+    @Test
+    public void runTestGroup_array_constructor() throws Exception {
+        runTestGroup("array-constructor");
+    }
+    @Test
+    public void runTestGroup_blocks() throws Exception {
+        runTestGroup("blocks");
+    }
+    @Test
+    public void runTestGroup_boolean_expresssions() throws Exception {
+        runTestGroup("boolean-expresssions");
+    }
+    @Test
+    public void runTestGroup_closures() throws Exception {
+        runTestGroup("closures");
+    }
+    @Test
+    public void runTestGroup_comments() throws Exception {
+        runTestGroup("comments");
+    }
+    @Test
+    public void runTestGroup_comparison_operators() throws Exception {
+        runTestGroup("comparison-operators");
+    }
+    @Test
+    public void runTestGroup_conditionals() throws Exception {
+        runTestGroup("conditionals");
+    }
+    @Test
+    public void runTestGroup_context() throws Exception {
+        runTestGroup("context");
+    }
+    @Test
+    public void runTestGroup_descendent_operator() throws Exception {
+        runTestGroup("descendent-operator");
+    }
+    @Test
+    public void runTestGroup_encoding() throws Exception {
+        runTestGroup("encoding");
+    }
+    @Test
+    public void runTestGroup_errors() throws Exception {
+        runTestGroup("errors");
+    }
+    @Test
+    public void runTestGroup_fields() throws Exception {
+        runTestGroup("fields");
+    }
+    @Test
+    public void runTestGroup_flattening() throws Exception {
+        runTestGroup("flattening");
+    }
+    @Test
+    public void runTestGroup_function_abs() throws Exception {
+        runTestGroup("function-abs");
+    }
+    @Test
+    public void runTestGroup_function_append() throws Exception {
+        runTestGroup("function-append");
+    }
+    @Test
+    public void runTestGroup_function_applications() throws Exception {
+        runTestGroup("function-applications");
+    }
+    @Test
+    public void runTestGroup_function_assert() throws Exception {
+        runTestGroup("function-assert");
+    }
+    @Test
+    public void runTestGroup_function_average() throws Exception {
+        runTestGroup("function-average");
+    }
+    @Test
+    public void runTestGroup_function_boolean() throws Exception {
+        runTestGroup("function-boolean");
+    }
+    @Test
+    public void runTestGroup_function_ceil() throws Exception {
+        runTestGroup("function-ceil");
+    }
+    @Test
+    public void runTestGroup_function_contains() throws Exception {
+        runTestGroup("function-contains");
+    }
+    @Test
+    public void runTestGroup_function_count() throws Exception {
+        runTestGroup("function-count");
+    }
+    @Test
+    public void runTestGroup_function_decodeUrl() throws Exception {
+        runTestGroup("function-decodeUrl");
+    }
+    @Test
+    public void runTestGroup_function_decodeUrlComponent() throws Exception {
+        runTestGroup("function-decodeUrlComponent");
+    }
+    @Test
+    public void runTestGroup_function_distinct() throws Exception {
+        runTestGroup("function-distinct");
+    }
+    @Test
+    public void runTestGroup_function_each() throws Exception {
+        runTestGroup("function-each");
+    }
+    @Test
+    public void runTestGroup_function_encodeUrl() throws Exception {
+        runTestGroup("function-encodeUrl");
+    }
+    @Test
+    public void runTestGroup_function_encodeUrlComponent() throws Exception {
+        runTestGroup("function-encodeUrlComponent");
+    }
+    @Test
+    public void runTestGroup_function_error() throws Exception {
+        runTestGroup("function-error");
+    }
+    @Test
+    public void runTestGroup_function_eval() throws Exception {
+        runTestGroup("function-eval");
+    }
+    @Test
+    public void runTestGroup_function_exists() throws Exception {
+        runTestGroup("function-exists");
+    }
+    @Test
+    public void runTestGroup_function_floor() throws Exception {
+        runTestGroup("function-floor");
+    }
+    @Test
+    public void runTestGroup_function_formatBase() throws Exception {
+        runTestGroup("function-formatBase");
+    }
+    @Test
+    public void runTestGroup_function_formatInteger() throws Exception {
+        runTestGroup("function-formatInteger");
+    }
+    @Test
+    public void runTestGroup_function_formatNumber() throws Exception {
+        runTestGroup("function-formatNumber");
+    }
+    @Test
+    public void runTestGroup_function_fromMillis() throws Exception {
+        runTestGroup("function-fromMillis");
+    }
+    @Test
+    public void runTestGroup_function_join() throws Exception {
+        runTestGroup("function-join");
+    }
+    @Test
+    public void runTestGroup_function_keys() throws Exception {
+        runTestGroup("function-keys");
+    }
+    @Test
+    public void runTestGroup_function_length() throws Exception {
+        runTestGroup("function-length");
+    }
+    @Test
+    public void runTestGroup_function_lookup() throws Exception {
+        runTestGroup("function-lookup");
+    }
+    @Test
+    public void runTestGroup_function_lowercase() throws Exception {
+        runTestGroup("function-lowercase");
+    }
+    @Test
+    public void runTestGroup_function_max() throws Exception {
+        runTestGroup("function-max");
+    }
+    @Test
+    public void runTestGroup_function_merge() throws Exception {
+        runTestGroup("function-merge");
+    }
+    @Test
+    public void runTestGroup_function_number() throws Exception {
+        runTestGroup("function-number");
+    }
+    @Test
+    public void runTestGroup_function_pad() throws Exception {
+        runTestGroup("function-pad");
+    }
+    @Test
+    public void runTestGroup_function_parseInteger() throws Exception {
+        runTestGroup("function-parseInteger");
+    }
+    @Test
+    public void runTestGroup_function_power() throws Exception {
+        runTestGroup("function-power");
+    }
+    @Test
+    public void runTestGroup_function_replace() throws Exception {
+        runTestGroup("function-replace");
+    }
+    @Test
+    public void runTestGroup_function_reverse() throws Exception {
+        runTestGroup("function-reverse");
+    }
+    @Test
+    public void runTestGroup_function_round() throws Exception {
+        runTestGroup("function-round");
+    }
+    @Test
+    public void runTestGroup_function_shuffle() throws Exception {
+        runTestGroup("function-shuffle");
+    }
+    @Test
+    public void runTestGroup_function_sift() throws Exception {
+        runTestGroup("function-sift");
+    }
+    @Test
+    public void runTestGroup_function_signatures() throws Exception {
+        runTestGroup("function-signatures");
+    }
+    @Test
+    public void runTestGroup_function_sort() throws Exception {
+        runTestGroup("function-sort");
+    }
+    @Test
+    public void runTestGroup_function_split() throws Exception {
+        runTestGroup("function-split");
+    }
+    @Test
+    public void runTestGroup_function_spread() throws Exception {
+        runTestGroup("function-spread");
+    }
+    @Test
+    public void runTestGroup_function_sqrt() throws Exception {
+        runTestGroup("function-sqrt");
+    }
+    @Test
+    public void runTestGroup_function_string() throws Exception {
+        runTestGroup("function-string");
+    }
+    @Test
+    public void runTestGroup_function_substring() throws Exception {
+        runTestGroup("function-substring");
+    }
+    @Test
+    public void runTestGroup_function_substringAfter() throws Exception {
+        runTestGroup("function-substringAfter");
+    }
+    @Test
+    public void runTestGroup_function_substringBefore() throws Exception {
+        runTestGroup("function-substringBefore");
+    }
+    @Test
+    public void runTestGroup_function_sum() throws Exception {
+        runTestGroup("function-sum");
+    }
+    @Test
+    public void runTestGroup_function_tomillis() throws Exception {
+        runTestGroup("function-tomillis");
+    }
+    @Test
+    public void runTestGroup_function_trim() throws Exception {
+        runTestGroup("function-trim");
+    }
+    @Test
+    public void runTestGroup_function_typeOf() throws Exception {
+        runTestGroup("function-typeOf");
+    }
+    @Test
+    public void runTestGroup_function_uppercase() throws Exception {
+        runTestGroup("function-uppercase");
+    }
+    @Test
+    public void runTestGroup_function_zip() throws Exception {
+        runTestGroup("function-zip");
+    }
+    @Test
+    public void runTestGroup_higher_order_functions() throws Exception {
+        runTestGroup("higher-order-functions");
+    }
+    @Test
+    public void runTestGroup_hof_filter() throws Exception {
+        runTestGroup("hof-filter");
+    }
+    @Test
+    public void runTestGroup_hof_map() throws Exception {
+        runTestGroup("hof-map");
+    }
+    @Test
+    public void runTestGroup_hof_reduce() throws Exception {
+        runTestGroup("hof-reduce");
+    }
+    @Test
+    public void runTestGroup_hof_single() throws Exception {
+        runTestGroup("hof-single");
+    }
+    @Test
+    public void runTestGroup_hof_zip_map() throws Exception {
+        runTestGroup("hof-zip-map");
+    }
+    @Test
+    public void runTestGroup_inclusion_operator() throws Exception {
+        runTestGroup("inclusion-operator");
+    }
+    @Test
+    public void runTestGroup_joins() throws Exception {
+        runTestGroup("joins");
+    }
+    @Test
+    public void runTestGroup_lambdas() throws Exception {
+        runTestGroup("lambdas");
+    }
+    @Test
+    public void runTestGroup_literals() throws Exception {
+        runTestGroup("literals");
+    }
+    @Test
+    public void runTestGroup_matchers() throws Exception {
+        runTestGroup("matchers");
+    }
+    @Test
+    public void runTestGroup_missing_paths() throws Exception {
+        runTestGroup("missing-paths");
+    }
+    @Test
+    public void runTestGroup_multiple_array_selectors() throws Exception {
+        runTestGroup("multiple-array-selectors");
+    }
+    @Test
+    public void runTestGroup_null() throws Exception {
+        runTestGroup("null");
+    }
+    @Test
+    public void runTestGroup_numeric_operators() throws Exception {
+        runTestGroup("numeric-operators");
+    }
+    @Test
+    public void runTestGroup_object_constructor() throws Exception {
+        runTestGroup("object-constructor");
+    }
+    @Test
+    public void runTestGroup_parent_operator() throws Exception {
+        runTestGroup("parent-operator");
+    }
+    @Test
+    public void runTestGroup_parentheses() throws Exception {
+        runTestGroup("parentheses");
+    }
+    @Test
+    public void runTestGroup_partial_application() throws Exception {
+        runTestGroup("partial-application");
+    }
+    @Test
+    public void runTestGroup_performance() throws Exception {
+        runTestGroup("performance");
+    }
+    @Test
+    public void runTestGroup_predicates() throws Exception {
+        runTestGroup("predicates");
+    }
+    @Test
+    public void runTestGroup_quoted_selectors() throws Exception {
+        runTestGroup("quoted-selectors");
+    }
+    @Test
+    public void runTestGroup_range_operator() throws Exception {
+        runTestGroup("range-operator");
+    }
+    @Test
+    public void runTestGroup_regex() throws Exception {
+        runTestGroup("regex");
+    }
+    @Test
+    public void runTestGroup_simple_array_selectors() throws Exception {
+        runTestGroup("simple-array-selectors");
+    }
+    @Test
+    public void runTestGroup_sorting() throws Exception {
+        runTestGroup("sorting");
+    }
+    @Test
+    public void runTestGroup_string_concat() throws Exception {
+        runTestGroup("string-concat");
+    }
+    @Test
+    public void runTestGroup_tail_recursion() throws Exception {
+        runTestGroup("tail-recursion");
+    }
+    @Test
+    public void runTestGroup_token_conversion() throws Exception {
+        runTestGroup("token-conversion");
+    }
+    @Test
+    public void runTestGroup_transform() throws Exception {
+        runTestGroup("transform");
+    }
+    @Test
+    public void runTestGroup_transforms() throws Exception {
+        runTestGroup("transforms");
+    }
+    @Test
+    public void runTestGroup_variables() throws Exception {
+        runTestGroup("variables");
+    }
+    @Test
+    public void runTestGroup_wildcards() throws Exception {
+        runTestGroup("wildcards");
+    }
+}
