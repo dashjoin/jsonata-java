f0bd08724e5aa21fdb2e720693b22e3915022c83
uw4 <uli.walther@gmail.com>
2023-07-03 17:43:12 +0200

    Fix many formatNumber tests

--- src/main/java/com/dashjoin/jsonata/utils/Constants.java
+++ src/main/java/com/dashjoin/jsonata/utils/Constants.java
@@ -11,4 +11,39 @@ public class Constants {
     public static final String ERR_MSG_INVALID_NAME_MODIFIER = "The 'name' modifier can only be applied to months and days in the date/time picture string, not %s";
     public static final String ERR_MSG_TIMEZONE_FORMAT = "The timezone integer format specifier cannot have more than four digits";
     public static final String ERR_MSG_MISSING_FORMAT = "The date/time picture string is missing specifiers required to parse the timestamp";
+    public static final String ERR_MSG_INVALID_OPTIONS_SINGLE_CHAR = "Argument 3 of function %s is invalid. The value of the %s property must be a single character";
+    public static final String ERR_MSG_INVALID_OPTIONS_STRING = "Argument 3 of function %s is invalid. The value of the %s property must be a string";
+
+    /**
+    * Collection of decimal format strings that defined by xsl:decimal-format.
+    * 
+    * <pre>
+    *     &lt;!ELEMENT xsl:decimal-format EMPTY&gt;
+    *     &lt;!ATTLIST xsl:decimal-format
+    *       name %qname; #IMPLIED
+    *       decimal-separator %char; "."
+    *       grouping-separator %char; ","
+    *       infinity CDATA "Infinity"
+    *       minus-sign %char; "-"
+    *       NaN CDATA "NaN"
+    *       percent %char; "%"
+    *       per-mille %char; "&#x2030;"
+    *       zero-digit %char; "0"
+    *       digit %char; "#"
+    *       pattern-separator %char; ";"&GT;
+    * </pre>
+    * 
+    * http://www.w3.org/TR/xslt#format-number} to explain format-number in XSLT
+    *      Specification xsl.usage advanced
+    */
+    public static final String SYMBOL_DECIMAL_SEPARATOR = "decimal-separator";
+    public static final String SYMBOL_GROUPING_SEPARATOR = "grouping-separator";
+    public static final String SYMBOL_INFINITY = "infinity";
+    public static final String SYMBOL_MINUS_SIGN = "minus-sign";
+    public static final String SYMBOL_NAN = "NaN";
+    public static final String SYMBOL_PERCENT = "percent";
+    public static final String SYMBOL_PER_MILLE = "per-mille";
+    public static final String SYMBOL_ZERO_DIGIT = "zero-digit";
+    public static final String SYMBOL_DIGIT = "digit";
+    public static final String SYMBOL_PATTERN_SEPARATOR = "pattern-separator";
 }
