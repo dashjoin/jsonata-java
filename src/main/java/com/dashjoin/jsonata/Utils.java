b544f8b471a14996b5728cf9b6a9e9ca86b063ed
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2023-07-03 17:50:03 +0200

    JList.toString() no longer needed - handled by $string

--- src/main/java/com/dashjoin/jsonata/Utils.java
+++ src/main/java/com/dashjoin/jsonata/Utils.java
@@ -80,22 +80,6 @@ public class Utils {
         public boolean keepSingleton;
 
         public boolean cons;
-        
-        public String toString() {
-          Iterator<E> it = iterator();
-          if (! it.hasNext())
-              return "[]";
-
-          StringBuilder sb = new StringBuilder();
-          sb.append('[');
-          for (;;) {
-              E e = it.next();
-              sb.append(e == this ? "(this Collection)" : e);
-              if (! it.hasNext())
-                  return sb.append(']').toString();
-              sb.append(',') /* .append(' ') */;
-          }
-        }
     }
 
     public static boolean isSequence(Object result) {
