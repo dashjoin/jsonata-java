5860e5ef82098608e1a56f577d2011033ee4f148
uw4 <uli.walther@gmail.com>
2023-07-18 18:34:43 +0200

    Generate error messages from error code
    + deactivated warnings

--- src/main/java/com/dashjoin/jsonata/JException.java
+++ src/main/java/com/dashjoin/jsonata/JException.java
@@ -1,5 +1,6 @@
 package com.dashjoin.jsonata;
 
+import java.util.IllegalFormatException;
 import java.util.List;
 
 public class JException extends Exception {
@@ -10,19 +11,57 @@ public class JException extends Exception {
     Object expected;
 
     public JException(String error, int location) {
-        super("JSonataException "+error+" @"+location);
+        super(msg(error, location, null, null));
         this.error = error; this.location = location;
     }
     public JException(String error, int location, Object currentToken) {
-        super("JSonataException "+error+" @"+location+" current="+currentToken);
+        super(msg(error, location, currentToken, null));
         this.error = error; this.location = location;
         this.current = currentToken;
     }
     public JException(String error, int location, Object currentToken, Object expected) {
-        super("JSonataException "+error+" @"+location+" current="+currentToken+" expected="+expected);
+        super(msg(error, location, currentToken, expected));
         this.error = error; this.location = location;
         this.current = currentToken;
         this.expected = expected;
+    }
+
+    /**
+     * Generate error message from given error code
+     * Codes are defined in Jsonata.errorCodes
+     * 
+     * Fallback: if error code does not exist, return a generic message
+     * 
+     * @param error
+     * @param location
+     * @param arg1
+     * @param arg2
+     * @return
+     */
+    public static String msg(String error, int location, Object arg1, Object arg2) {
+        String message = Jsonata.errorCodes.get(error);
+
+        if (message==null) {
+            // unknown error code
+            return "JSonataException "+error+" @"+location+" arg1="+arg1+" arg2="+arg2;
+        }
+
+        String formatted = message;
+        try {
+            formatted = formatted.replace("{{index}}", "%1s")
+                .replace("{{token}}", "%2s")
+                .replace("{{{message}}}", "%1s")
+                .replace("{{value}}", "%1s");
+
+            formatted = String.format(formatted, arg1, arg2);
+        } catch (IllegalFormatException ex) {
+            ex.printStackTrace();
+            // ignore
+        }
+        formatted = error+": "+formatted;
+        if (location>=0)
+            formatted += ". Location="+location;
+        return formatted;
     }
 
     // Recover
--- src/main/java/com/dashjoin/jsonata/Jsonata.java
+++ src/main/java/com/dashjoin/jsonata/Jsonata.java
@@ -29,6 +29,7 @@ import com.dashjoin.jsonata.utils.Signature;
  * @module JSONata
  * @description JSON query and transformation language
  */
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class Jsonata {
 
  /**
@@ -2276,102 +2277,102 @@ public class Jsonata {
       *  3xxx    - functions (blocks of 10 for each function)
       */
      static HashMap<String,String> errorCodes = new HashMap<>() {{
-         put("S0101", "String literal must be terminated by a matching quote");
-        //  "S0102": "Number out of range: {{token}}",
-        //  "S0103": "Unsupported escape sequence: \\{{token}}",
-        //  "S0104": "The escape sequence \\u must be followed by 4 hex digits",
-        //  "S0105": "Quoted property name must be terminated with a backquote (`)",
-        //  "S0106": "Comment has no closing tag",
-        //  "S0201": "Syntax error: {{token}}",
-        //  "S0202": "Expected {{value}}, got {{token}}",
-        //  "S0203": "Expected {{value}} before end of expression",
-        //  "S0204": "Unknown operator: {{token}}",
-        //  "S0205": "Unexpected token: {{token}}",
-        //  "S0206": "Unknown expression type: {{token}}",
-        //  "S0207": "Unexpected end of expression",
-        //  "S0208": "Parameter {{value}} of Object definition must be a variable name (start with $)",
-        //  "S0209": "A predicate cannot follow a grouping expression in a step",
-        //  "S0210": "Each step can only have one grouping expression",
-        //  "S0211": "The symbol {{token}} cannot be used as a unary operator",
-        //  "S0212": "The left side of := must be a variable name (start with $)",
-        //  "S0213": "The literal value {{value}} cannot be used as a step within a path expression",
-        //  "S0214": "The right side of {{token}} must be a variable name (start with $)",
-        //  "S0215": "A context variable binding must precede any predicates on a step",
-        //  "S0216": "A context variable binding must precede the "order-by" clause on a step",
-        //  "S0217": "The object representing the "parent" cannot be derived from this expression",
-        //  "S0301": "Empty regular expressions are not allowed",
-        //  "S0302": "No terminating / in regular expression",
-        //  "S0402": "Choice groups containing parameterized types are not supported",
-        //  "S0401": "Type parameters can only be applied to functions and arrays",
-        //  "S0500": "Attempted to evaluate an expression containing syntax error(s)",
-        //  "T0410": "Argument {{index}} of Object {{token}} does not match Object signature",
-        //  "T0411": "Context value is not a compatible type with argument {{index}} of Object {{token}}",
-        //  "T0412": "Argument {{index}} of Object {{token}} must be an array of {{type}}",
-        //  "D1001": "Number out of range: {{value}}",
-        //  "D1002": "Cannot negate a non-numeric value: {{value}}",
-        //  "T1003": "Key in object structure must evaluate to a string; got: {{value}}",
-        //  "D1004": "Regular expression matches zero length string",
-        //  "T1005": "Attempted to invoke a non-function. Did you mean ${{{token}}}?",
-        //  "T1006": "Attempted to invoke a non-function",
-        //  "T1007": "Attempted to partially apply a non-function. Did you mean ${{{token}}}?",
-        //  "T1008": "Attempted to partially apply a non-function",
-        //  "D1009": "Multiple key definitions evaluate to same key: {{value}}",
-        //  "T1010": "The matcher Object argument passed to Object {{token}} does not return the correct object structure",
-        //  "T2001": "The left side of the {{token}} operator must evaluate to a number",
-        //  "T2002": "The right side of the {{token}} operator must evaluate to a number",
-        //  "T2003": "The left side of the range operator (..) must evaluate to an integer",
-        //  "T2004": "The right side of the range operator (..) must evaluate to an integer",
-        //  "D2005": "The left side of := must be a variable name (start with $)",  // defunct - replaced by S0212 parser error
-        //  "T2006": "The right side of the Object application operator ~> must be a function",
-        //  "T2007": "Type mismatch when comparing values {{value}} and {{value2}} in order-by clause",
-        //  "T2008": "The expressions within an order-by clause must evaluate to numeric or string values",
-        //  "T2009": "The values {{value}} and {{value2}} either side of operator {{token}} must be of the same data type",
-        //  "T2010": "The expressions either side of operator {{token}} must evaluate to numeric or string values",
-        //  "T2011": "The insert/update clause of the transform expression must evaluate to an object: {{value}}",
-        //  "T2012": "The delete clause of the transform expression must evaluate to a string or array of strings: {{value}}",
-        //  "T2013": "The transform expression clones the input object using the $clone() function.  This has been overridden in the current scope by a non-function.",
-        //  "D2014": "The size of the sequence allocated by the range operator (..) must not exceed 1e6.  Attempted to allocate {{value}}.",
-        //  "D3001": "Attempting to invoke string Object on Infinity or NaN",
-        //  "D3010": "Second argument of replace Object cannot be an empty string",
-        //  "D3011": "Fourth argument of replace Object must evaluate to a positive number",
-        //  "D3012": "Attempted to replace a matched string with a non-string value",
-        //  "D3020": "Third argument of split Object must evaluate to a positive number",
-        //  "D3030": "Unable to cast value to a number: {{value}}",
-        //  "D3040": "Third argument of match Object must evaluate to a positive number",
-        //  "D3050": "The second argument of reduce Object must be a Object with at least two arguments",
-        //  "D3060": "The sqrt Object cannot be applied to a negative number: {{value}}",
-        //  "D3061": "The power Object has resulted in a value that cannot be represented as a JSON number: base={{value}}, exponent={{exp}}",
-        //  "D3070": "The single argument form of the sort Object can only be applied to an array of strings or an array of numbers.  Use the second argument to specify a comparison function",
-        //  "D3080": "The picture string must only contain a maximum of two sub-pictures",
-        //  "D3081": "The sub-picture must not contain more than one instance of the "decimal-separator" character",
-        //  "D3082": "The sub-picture must not contain more than one instance of the "percent" character",
-        //  "D3083": "The sub-picture must not contain more than one instance of the "per-mille" character",
-        //  "D3084": "The sub-picture must not contain both a "percent" and a "per-mille" character",
-        //  "D3085": "The mantissa part of a sub-picture must contain at least one character that is either an "optional digit character" or a member of the "decimal digit family"",
-        //  "D3086": "The sub-picture must not contain a passive character that is preceded by an active character and that is followed by another active character",
-        //  "D3087": "The sub-picture must not contain a "grouping-separator" character that appears adjacent to a "decimal-separator" character",
-        //  "D3088": "The sub-picture must not contain a "grouping-separator" at the end of the integer part",
-        //  "D3089": "The sub-picture must not contain two adjacent instances of the "grouping-separator" character",
-        //  "D3090": "The integer part of the sub-picture must not contain a member of the "decimal digit family" that is followed by an instance of the "optional digit character"",
-        //  "D3091": "The fractional part of the sub-picture must not contain an instance of the "optional digit character" that is followed by a member of the "decimal digit family"",
-        //  "D3092": "A sub-picture that contains a "percent" or "per-mille" character must not contain a character treated as an "exponent-separator"",
-        //  "D3093": "The exponent part of the sub-picture must comprise only of one or more characters that are members of the "decimal digit family"",
-        //  "D3100": "The radix of the formatBase Object must be between 2 and 36.  It was given {{value}}",
-        //  "D3110": "The argument of the toMillis Object must be an ISO 8601 formatted timestamp. Given {{value}}",
-        //  "D3120": "Syntax error in expression passed to Object eval: {{value}}",
-        //  "D3121": "Dynamic error evaluating the expression passed to Object eval: {{value}}",
-        //  "D3130": "Formatting or parsing an integer as a sequence starting with {{value}} is not supported by this implementation",
-        //  "D3131": "In a decimal digit pattern, all digits must be from the same decimal group",
-        //  "D3132": "Unknown component specifier {{value}} in date/time picture string",
-        //  "D3133": "The "name" modifier can only be applied to months and days in the date/time picture string, not {{value}}",
-        //  "D3134": "The timezone integer format specifier cannot have more than four digits",
-        //  "D3135": "No matching closing bracket "]" in date/time picture string",
-        //  "D3136": "The date/time picture string is missing specifiers required to parse the timestamp",
-        //  "D3137": "{{{message}}}",
-        //  "D3138": "The $single() Object expected exactly 1 matching result.  Instead it matched more.",
-        //  "D3139": "The $single() Object expected exactly 1 matching result.  Instead it matched 0.",
-        //  "D3140": "Malformed URL passed to ${{{functionName}}}(): {{value}}",
-        //  "D3141": "{{{message}}}"
+        put("S0101", "String literal must be terminated by a matching quote");
+        put("S0102", "Number out of range: {{token}}");
+        put("S0103", "Unsupported escape sequence: \\{{token}}");
+        put("S0104", "The escape sequence \\u must be followed by 4 hex digits");
+        put("S0105", "Quoted property name must be terminated with a backquote (`)");
+        put("S0106", "Comment has no closing tag");
+        put("S0201", "Syntax error: {{token}}");
+        put("S0202", "Expected {{value}}, got {{token}}");
+        put("S0203", "Expected {{value}} before end of expression");
+        put("S0204", "Unknown operator: {{token}}");
+        put("S0205", "Unexpected token: {{token}}");
+        put("S0206", "Unknown expression type: {{token}}");
+        put("S0207", "Unexpected end of expression");
+        put("S0208", "Parameter {{value}} of Object definition must be a variable name (start with $)");
+        put("S0209", "A predicate cannot follow a grouping expression in a step");
+        put("S0210", "Each step can only have one grouping expression");
+        put("S0211", "The symbol {{token}} cannot be used as a unary operator");
+        put("S0212", "The left side of := must be a variable name (start with $)");
+        put("S0213", "The literal value {{value}} cannot be used as a step within a path expression");
+        put("S0214", "The right side of {{token}} must be a variable name (start with $)");
+        put("S0215", "A context variable binding must precede any predicates on a step");
+        put("S0216", "A context variable binding must precede the \"order-by\" clause on a step");
+        put("S0217", "The object representing the \"parent\" cannot be derived from this expression");
+        put("S0301", "Empty regular expressions are not allowed");
+        put("S0302", "No terminating / in regular expression");
+        put("S0402", "Choice groups containing parameterized types are not supported");
+        put("S0401", "Type parameters can only be applied to functions and arrays");
+        put("S0500", "Attempted to evaluate an expression containing syntax error(s)");
+        put("T0410", "Argument {{index}} of Object {{token}} does not match Object signature");
+        put("T0411", "Context value is not a compatible type with argument {{index}} of Object {{token}}");
+        put("T0412", "Argument {{index}} of Object {{token}} must be an array of {{type}}");
+        put("D1001", "Number out of range: {{value}}");
+        put("D1002", "Cannot negate a non-numeric value: {{value}}");
+        put("T1003", "Key in object structure must evaluate to a string; got: {{value}}");
+        put("D1004", "Regular expression matches zero length string");
+        put("T1005", "Attempted to invoke a non-function. Did you mean ${{{token}}}?");
+        put("T1006", "Attempted to invoke a non-function");
+        put("T1007", "Attempted to partially apply a non-function. Did you mean ${{{token}}}?");
+        put("T1008", "Attempted to partially apply a non-function");
+        put("D1009", "Multiple key definitions evaluate to same key: {{value}}");
+        put("T1010", "The matcher Object argument passed to Object {{token}} does not return the correct object structure");
+        put("T2001", "The left side of the {{token}} operator must evaluate to a number");
+        put("T2002", "The right side of the {{token}} operator must evaluate to a number");
+        put("T2003", "The left side of the range operator (..) must evaluate to an integer");
+        put("T2004", "The right side of the range operator (..) must evaluate to an integer");
+        put("D2005", "The left side of := must be a variable name (start with $)");  // defunct - replaced by S0212 parser error
+        put("T2006", "The right side of the Object application operator ~> must be a function");
+        put("T2007", "Type mismatch when comparing values {{value}} and {{value2}} in order-by clause");
+        put("T2008", "The expressions within an order-by clause must evaluate to numeric or string values");
+        put("T2009", "The values {{value}} and {{value2}} either side of operator {{token}} must be of the same data type");
+        put("T2010", "The expressions either side of operator {{token}} must evaluate to numeric or string values");
+        put("T2011", "The insert/update clause of the transform expression must evaluate to an object: {{value}}");
+        put("T2012", "The delete clause of the transform expression must evaluate to a string or array of strings: {{value}}");
+        put("T2013", "The transform expression clones the input object using the $clone() function.  This has been overridden in the current scope by a non-function.");
+        put("D2014", "The size of the sequence allocated by the range operator (..) must not exceed 1e6.  Attempted to allocate {{value}}.");
+        put("D3001", "Attempting to invoke string Object on Infinity or NaN");
+        put("D3010", "Second argument of replace Object cannot be an empty string");
+        put("D3011", "Fourth argument of replace Object must evaluate to a positive number");
+        put("D3012", "Attempted to replace a matched string with a non-string value");
+        put("D3020", "Third argument of split Object must evaluate to a positive number");
+        put("D3030", "Unable to cast value to a number: {{value}}");
+        put("D3040", "Third argument of match Object must evaluate to a positive number");
+        put("D3050", "The second argument of reduce Object must be a Object with at least two arguments");
+        put("D3060", "The sqrt Object cannot be applied to a negative number: {{value}}");
+        put("D3061", "The power Object has resulted in a value that cannot be represented as a JSON number: base={{value}}, exponent={{exp}}");
+        put("D3070", "The single argument form of the sort Object can only be applied to an array of strings or an array of numbers.  Use the second argument to specify a comparison function");
+        put("D3080", "The picture string must only contain a maximum of two sub-pictures");
+        put("D3081", "The sub-picture must not contain more than one instance of the \"decimal-separator\" character");
+        put("D3082", "The sub-picture must not contain more than one instance of the \"percent\" character");
+        put("D3083", "The sub-picture must not contain more than one instance of the \"per-mille\" character");
+        put("D3084", "The sub-picture must not contain both a \"percent\" and a \"per-mille\" character");
+        put("D3085", "The mantissa part of a sub-picture must contain at least one character that is either an \"optional digit character\" or a member of the \"decimal digit family\"");
+        put("D3086", "The sub-picture must not contain a passive character that is preceded by an active character and that is followed by another active character");
+        put("D3087", "The sub-picture must not contain a \"grouping-separator\" character that appears adjacent to a \"decimal-separator\" character");
+        put("D3088", "The sub-picture must not contain a \"grouping-separator\" at the end of the integer part");
+        put("D3089", "The sub-picture must not contain two adjacent instances of the \"grouping-separator\" character");
+        put("D3090", "The integer part of the sub-picture must not contain a member of the \"decimal digit family\" that is followed by an instance of the \"optional digit character\"");
+        put("D3091", "The fractional part of the sub-picture must not contain an instance of the \"optional digit character\" that is followed by a member of the \"decimal digit family\"");
+        put("D3092", "A sub-picture that contains a \"percent\" or \"per-mille\" character must not contain a character treated as an \"exponent-separator\"");
+        put("D3093", "The exponent part of the sub-picture must comprise only of one or more characters that are members of the \"decimal digit family\"");
+        put("D3100", "The radix of the formatBase Object must be between 2 and 36.  It was given {{value}}");
+        put("D3110", "The argument of the toMillis Object must be an ISO 8601 formatted timestamp. Given {{value}}");
+        put("D3120", "Syntax error in expression passed to Object eval: {{value}}");
+        put("D3121", "Dynamic error evaluating the expression passed to Object eval: {{value}}");
+        put("D3130", "Formatting or parsing an integer as a sequence starting with {{value}} is not supported by this implementation");
+        put("D3131", "In a decimal digit pattern, all digits must be from the same decimal group");
+        put("D3132", "Unknown component specifier {{value}} in date/time picture string");
+        put("D3133", "The \"name\" modifier can only be applied to months and days in the date/time picture string, not {{value}}");
+        put("D3134", "The timezone integer format specifier cannot have more than four digits");
+        put("D3135", "No matching closing bracket \"]\" in date/time picture string");
+        put("D3136", "The date/time picture string is missing specifiers required to parse the timestamp");
+        put("D3137", "{{{message}}}");
+        put("D3138", "The $single() Object expected exactly 1 matching result.  Instead it matched more.");
+        put("D3139", "The $single() Object expected exactly 1 matching result.  Instead it matched 0.");
+        put("D3140", "Malformed URL passed to ${{{functionName}}}(): {{value}}");
+        put("D3141", "{{{message}}}");
      }};
  
      /**
