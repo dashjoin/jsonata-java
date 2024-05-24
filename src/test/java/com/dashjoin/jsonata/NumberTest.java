9f2106288174fce7cf6b543d1575e76613c5f37d
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2024-05-24 15:57:13 +0200

    simplify unit test

--- src/test/java/com/dashjoin/jsonata/NumberTest.java
+++ src/test/java/com/dashjoin/jsonata/NumberTest.java
@@ -1,7 +1,6 @@
 package com.dashjoin.jsonata;
 
 import static com.dashjoin.jsonata.Jsonata.jsonata;
-import static java.util.Arrays.asList;
 import static java.util.Map.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Disabled;
@@ -14,15 +13,13 @@ public class NumberTest {
   public void testDouble() {
     Jsonata expr1 = jsonata("x");
     var res = expr1.evaluate(of("x", 1.0));
-    System.out.println(res);
     assertEquals(1, res);
   }
   
   @Test
   public void testConst() {
     Jsonata expr1 = jsonata("1.0");
-    var res = expr1.evaluate(of("key", asList(of("x", "y"), of("a", "b"))));
-    System.out.println(res);
+    var res = expr1.evaluate(null);
     assertEquals(1, res);
   }
 }
