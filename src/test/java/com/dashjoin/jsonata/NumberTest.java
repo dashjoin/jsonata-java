8b536faec4db62c31bf5422caad9fe50fca81cca
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2024-05-24 18:22:02 +0200

    more documentation / test cases for #40

--- src/test/java/com/dashjoin/jsonata/NumberTest.java
+++ src/test/java/com/dashjoin/jsonata/NumberTest.java
@@ -5,9 +5,13 @@ import static java.util.Map.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import com.dashjoin.jsonata.json.Json;
 
 public class NumberTest {
 
+  /**
+   * this case fails, because the double value 1.0 is "untouched
+   */
   @Disabled
   @Test
   public void testDouble() {
@@ -16,6 +20,26 @@ public class NumberTest {
     assertEquals(1, res);
   }
   
+  /**
+   * a computation is applied, and com.dashjoin.jsonata.Utils.convertNumber(Number) casts the double to int
+   */
+  @Test
+  public void testDouble2() {
+    Jsonata expr1 = jsonata("x+0");
+    var res = expr1.evaluate(of("x", 1.0));
+    assertEquals(1, res);
+  }
+
+  /**
+   * here, the JSON parser immediately converts double 1.0 to int 1
+   */
+  @Test
+  public void testDouble3() {
+    Jsonata expr1 = jsonata("x");
+    var res = expr1.evaluate(Json.parseJson("{\"x\":1.0}"));
+    assertEquals(1, res);
+  }
+      
   @Test
   public void testConst() {
     Jsonata expr1 = jsonata("1.0");
