071dabfb32422e8bd99d26275f3fe72380e47545
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2024-06-19 10:22:29 +0200

    Input fields - Expression to generate options, does not "know" variables #371

--- src/test/java/com/dashjoin/jsonata/ArrayTest.java
+++ src/test/java/com/dashjoin/jsonata/ArrayTest.java
@@ -4,6 +4,9 @@ import static com.dashjoin.jsonata.Jsonata.jsonata;
 import static java.util.Arrays.asList;
 import static java.util.Map.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Map;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ArrayTest {
@@ -15,5 +18,12 @@ public class ArrayTest {
     Jsonata expr2 = jsonata("{'key': $append($.[{'x': 'y'}],[{'a': 'b'}])}");
     var res2 = expr2.evaluate(of("key", asList(of("x", "y"), of("a", "b"))));
     assertEquals(res1, res2);
+  }
+  
+  @Disabled
+  @Test
+  public void filterTest() {
+    Jsonata expr = jsonata("($arr := [{'x':1}, {'x':2}];$arr[x=$number(variable.field)])");
+    Assertions.assertNotNull(expr.evaluate(Map.of("variable", Map.of("field", "1"))));
   }
 }
