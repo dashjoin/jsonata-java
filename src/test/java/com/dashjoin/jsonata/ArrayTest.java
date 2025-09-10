e220de0ceab5f712be0397ad3685d438a7793ed8
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2025-09-10 15:34:23 +0200

    Frame value not evaluated if used in array filter #45

--- src/test/java/com/dashjoin/jsonata/ArrayTest.java
+++ src/test/java/com/dashjoin/jsonata/ArrayTest.java
@@ -6,7 +6,6 @@ import static java.util.Map.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.Map;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class ArrayTest {
@@ -20,11 +19,10 @@ public class ArrayTest {
     assertEquals(res1, res2);
   }
   
-  @Disabled
   @Test
   public void filterTest() {
     // Frame value not evaluated if used in array filter #45
-    Jsonata expr = jsonata("($arr := [{'x':1}, {'x':2}];$arr[x=$number(variable.field)])");
+    Jsonata expr = jsonata("($arr := [{'x':1}, {'x':2}];$arr[x=$number($$.variable.field)])");
     Assertions.assertNotNull(expr.evaluate(Map.of("variable", Map.of("field", "1"))));
   }
 }
